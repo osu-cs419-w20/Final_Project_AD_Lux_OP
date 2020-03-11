7c0192a064ebdbded7d5de731272c14a3b46e998
jmanosu <jmanosu@gmail.com>
2020-03-11 15:47:22 -0700

    queried riot server correctly!

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,11 @@ import { css, jsx, Global } from '@emotion/core';
 
 import { useRouter } from 'next/router';
 
-function Home() {
+import fetch from 'isomorphic-unfetch';
+
+function Home ({ data }) {
+  
+  const router = useRouter();
   const styles = css`
     margin: 0px;
     padding: 0px;
@@ -26,15 +30,33 @@ function Home() {
     }
   `;
 
-  const router = useRouter();
+  async function handleClick() {
+    router.push('/summoner')
+  }
+
+  console.log(data);
 
   return (
     <div css={styles}>
-      <img src="https://images.wallpapersden.com/image/download/camille-league-of-legends_68170_1920x1133.jpg"/>
       <input type="text"></input>
-      <button onClick={() => router.push('/summoner')}>Search</button>
+      <button onClick={() => handleClick()}>Search</button>
+      <h1>
+        this is the data:
+      </h1>
     </div>
   );
+}
+
+Home.getInitialProps = async function (context) {
+  let responseBody = "";
+  console.log("quering for champions")
+  const url = 'https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-a3fce233-6c1f-4697-85c9-757a0cad7a7e';
+  const apiToken = "RGAPI-a3fce233-6c1f-4697-85c9-757a0cad7a7e";
+  const response = await fetch(url, {
+    method: 'GET'
+  });
+  responseBody = await response.json();
+  return { data: responseBody };
 }
 
 export default Home;
