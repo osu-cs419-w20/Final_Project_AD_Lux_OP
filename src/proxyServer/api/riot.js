4d0c8b6e2d87d8b90d63f28fc27d1789cd9e56e4
jmanosu <jmanosu@gmail.com>
2020-03-16 02:00:23 -0700

    minor changes

--- src/proxyServer/api/riot.js
+++ src/proxyServer/api/riot.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const request = require("request");
+const request = require('request');
 
 const riotBaseUrl = "https://na1.api.riotgames.com";
 const riotApiKey = "RGAPI-617f7d55-cd06-4471-8c9c-ec7492d1eb0c";
